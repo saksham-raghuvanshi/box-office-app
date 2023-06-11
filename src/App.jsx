b0dbd2956eb49ccd559dccb0fa29e7e53593889a
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-11 17:40:49 +0530

    react router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Notfound from "./Components/Notfound";
+
+import "./Styles/notfound.scss"
+import Starred from "./Components/Starred";
+
+
 function App() {
   return <div className="App">
-    
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/starred" element={<Starred/>} />
+        <Route path="*" element={<Notfound/>}/>
+
+        
+      </Routes>
+    </Router>
+
   </div>;
 }
 
---
+++ src/Components/Notfound.jsx
@@ -0,0 +1,11 @@
+
+
+const Notfound = () => {
+  return (
+    <div className='notfound'>
+      Page Not Found
+    </div>
+  )
+}
+
+export default Notfound
