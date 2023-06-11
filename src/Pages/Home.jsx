9b4d6dc78ca31d6597ce6149b53b17d4fa9223d7
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-11 20:25:06 +0530

    api conneted with form

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,26 +1,43 @@
- import { useState } from "react"
+import { useState } from 'react';
 
 const Home = () => {
-  const[inputValue, setInputValue]= useState(" ");
+  const [search, setSearch] = useState(' ');
 
-  console.log(inputValue);
-  const onInputChange = (ev) =>{
-setInputValue(ev.target.value);
-console.log(setInputValue)
+  console.log(search);
+  const onSearchInputChange = ev => {
+    //target means input value measns that enter check from dev console
+    setSearch(ev.target.value);
+    console.log(setSearch);
+  };
+
+  const onSearch = async ev => {
+    // for prevent loading on search
+    ev.preventDefault();
+
+    // fetch data from api // response
+    //await always work with async function
+    const response = await fetch(
+      'https://api.tvmaze.com/search/shows?q=${search}'
+    );
+    const body = await response.json();
+
+    console.log(body);
+
+    //
   };
   return (
     <div>
-
-      <div>{inputValue}</div>
-      <button onClick={()=>{
-        setInputValue("Saksham");
-      }}> Change Value
-      </button>
-      
-
-      <input type="text" value={inputValue} onChange={onInputChange} placeholder="Search for something"/>
+      <form onSubmit={onSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={onSearchInputChange}
+          placeholder="Search for something"
+        />
+        <button type="submit">Search</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
