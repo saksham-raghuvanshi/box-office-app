739ff38d86a1e2243fa50d2df8cdbc592e038444
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-18 21:41:01 +0530

    Custom React Hooks

--- src/Components/Show.jsx
+++ src/Components/Show.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getshowbyid } from '../Api/tvmaze';
-const Show = () => {
-  const { showid } = useParams();
+
+//Work Custom React Hook
+
+const useShowById = showid => {
   const [showData, setShowData] = useState(null);
   const [showError, setShowerror] = useState(null);
 
@@ -18,6 +20,29 @@ const Show = () => {
 
     fetchData();
   }, [showid]);
+
+  return { showData, showError };
+};
+const Show = () => {
+  const { showid } = useParams();
+  // to use custom hook react
+  const { showData, showError } = useShowById(showid);
+
+  // const [showData, setShowData] = useState(null);
+  // const [showError, setShowerror] = useState(null);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const data = await getshowbyid(showid);
+  //       setShowData(data);
+  //     } catch (err) {
+  //       setShowerror(err);
+  //     }
+  //   }
+
+  //   fetchData();
+  // }, [showid]);
 
   if (showError) {
     return <div>Error : {showError.message}</div>;
